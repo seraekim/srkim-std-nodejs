4754d8b8e7c43683deca44296a68c26515a9fb6e
Serae Kim <kserae@gmail.com>
2017-11-22 15:12:06 +0900

    http client get

---
+++ ex/ch05_07.js
@@ -0,0 +1,25 @@
+/**
+ * a basic nodejs http client requesting get method
+ */
+var http = require('http');
+
+var options = {
+    host: 'www.google.com',
+    port: 80,
+    path: '/'
+};
+
+var req = http.get(options, function(res) {
+    var resData = '';
+    res.on('data', function(chunk) {
+        resData += chunk;
+    });
+
+    res.on('end', function() {
+        console.log(resData);
+    });
+});
+
+req.on('error', function(err) {
+    console.log('error:', err.message);
+});
